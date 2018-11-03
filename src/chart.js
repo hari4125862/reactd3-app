c3b0846d312e1112a6aad07d97fa43ef53e17bbf
hari4125862 <hari4125862@gmail.com>
2018-11-03 19:17:49 +0530

    ploted graph for denny's working hours

--- src/chart.js
+++ src/chart.js
@@ -16,64 +16,64 @@ import { select } from "d3-selection"
            this.createBarChart()
         }
         createBarChart() {
-        //    const node = this.node
-        // //    const dataMax = max(this.props.data.hours)
-        //    const yScale = scaleLinear()
-        //       .domain([0, 100])
-        //       .range([0, this.props.size[1]])
-        //       const makeYLines = () => d3.axisLeft()
-        //       .scale(yScale)
+           const node = this.node
+        //    const dataMax = max(this.props.data.hours)
+           const yScale = scaleLinear()
+              .domain([0, 100])
+              .range([0, this.props.size[1]])
+              const makeYLines = () => d3.axisLeft()
+              .scale(yScale)
               
-        // select(node)
-        //    .selectAll('rect')
-        //    .data(this.props.data)
-        //    .enter()
-        //    .append('rect')
+        select(node)
+           .selectAll('rect')
+           .data(this.props.data)
+           .enter()
+           .append('rect')
         
-        // select(node)
-        //    .selectAll('rect')
-        //    .data(this.props.data)
-        //    .exit()
-        //    .remove()
+        select(node)
+           .selectAll('rect')
+           .data(this.props.data)
+           .exit()
+           .remove()
         
-        // select(node)
-        //    .selectAll('rect')
-        //    .data(this.props.data)
-        //    .style('fill', '#fe9922')
-        //    .attr('x', (d,i) => i * 25)
-        //    .attr('y', d => this.props.size[1] - yScale(d))
-        //    .attr('height', d => yScale(d))
-        //    .attr('width', 25)
+        select(node)
+           .selectAll('rect')
+           .data(this.props.data)
+           .style('fill', '#fe9922')
+           .attr('x', (d,i) => i * 25)
+           .attr('y', d => this.props.size[1] - yScale(d))
+           .attr('height', d => yScale(d))
+           .attr('width', 25)
 
-        const svg = d3.select('svg');
+        // const svg = d3.select('svg');
     
-        const margin = 100;
-        const width = 700 - 2 * margin;
-        const height = 500 - 2 * margin;
+        // const margin = 100;
+        // const width = 700 - 2 * margin;
+        // const height = 500 - 2 * margin;
     
-        const chart = svg.append('g')
-          .attr('transform', `translate(${margin}, ${margin})`);
+        // const chart = svg.append('g')
+        //   .attr('transform', `translate(${margin}, ${margin})`);
     
-        const xScale = d3.scaleBand()
-          .range([0, width])
-          .domain(this.props.data1.map((s) => s))
-          .padding(0.3)
+        // const xScale = d3.scaleBand()
+        //   .range([0, width])
+        //   .domain(this.props.data1.map((s) => s))
+        //   .padding(0.3)
         
-        const yScale = d3.scaleLinear()
-          .range([height, 0])
-          .domain([0, 100]);
+        // const yScale = d3.scaleLinear()
+        //   .range([height, 0])
+        //   .domain([0, 100]);
     
         
     
-        const makeYLines = () => d3.axisLeft()
-          .scale(yScale)
+        // const makeYLines = () => d3.axisLeft()
+        //   .scale(yScale)
     
-        chart.append('g')
-          .attr('transform', `translate(0, ${height})`)
-          .call(d3.axisBottom(xScale));
+        // chart.append('g')
+        //   .attr('transform', `translate(0, ${height})`)
+        //   .call(d3.axisBottom(xScale));
     
-        chart.append('g')
-          .call(d3.axisLeft(yScale));
+        // chart.append('g')
+        //   .call(d3.axisLeft(yScale));
     
            
         }
