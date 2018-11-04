fc8ebbdd8289ef8dff3b8d741931a6901a99e85b
hari4125862 <hari4125862@gmail.com>
2018-11-04 19:37:01 +0530

    updated routers for all 5 dataset and plotted daily working hours for all 5 workers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 import './App.css';
 import Extract from "./Extract";
-
+import Denny from "./Components/Denny";
+import Harry from "./Components/Harry";
+import Ho from "./Components/Ho";
+import Kay from "./Components/Kay";
+import Love from "./Components/Love";
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
        React + D3
-       <Extract/>
+
+       <Route exact path="/Denny" component={Denny} />
+          <Route exact path="/Harry" component={Harry} />
+          <Route exact path="/Ho" component={Ho} />
+          <Route exact path="/Kay" component={Kay} />
+          <Route exact path="/Love" component={Love} />
+<br/>
+
+ <Link className="nav-link" to="/Denny">
+ <button>Denny</button>
+  </Link>
+  <Link className="nav-link" to="/Harry">
+ <button>Harry</button>
+  </Link>
+  <Link className="nav-link" to="/Ho">
+ <button>Ho</button>
+  </Link>
+  <Link className="nav-link" to="/Kay">
+ <button>Kay</button>
+  </Link>
+  <Link className="nav-link" to="/Love">
+ <button>Love</button>
+  </Link>
+       {/* <Extract/> */}
       </div>
+      </BrowserRouter>
     );
   }
 }
--- src/Components/chart.js
+++ src/Components/chart.js
@@ -7,15 +7,17 @@ import React, { Component } from "react";
     class BarChart extends Component {
         constructor(props){
            super(props)
-           this.createBarChart = this.createBarChart.bind(this)
+           this.createDailyBarChart = this.createDailyBarChart.bind(this)
+           this.createMonthlyBarChart = this.createMonthlyBarChart.bind(this)
+
         }
         componentDidMount() {
-           this.createBarChart()
+          //  this.createBarChart()
         }
-        componentDidUpdate() {
-           this.createBarChart()
-        }
-        createBarChart() {
+        // componentDidUpdate() {
+        //    this.createBarChart()
+        // }
+        createDailyBarChart() {
         
 
         const svg = d3.select('svg');
@@ -95,10 +97,99 @@ import React, { Component } from "react";
   
     
     }
+
+    createMonthlyBarChart() {
+        
+
+      const svg = d3.select('svg');
+  
+      const margin = 100;
+      const width = 700 - 2 * margin;
+      const height = 500 - 2 * margin;
+  
+      const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+  
+      const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(this.props.data.map((s) => s.date))
+        .padding(0.3)
+      
+      const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 25]);
+  
+      const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+  
+      chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale))
+        .selectAll("text")  
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)");
+  
+      chart.append('g')
+        .call(d3.axisLeft(yScale));
+        chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+        .tickSize(-width, 0, 0)
+        .tickFormat('')
+        )
+        const barGroups = chart.selectAll()
+        .data(this.props.data)
+        .enter()
+        .append('g')
+  
+      barGroups
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', (g) => xScale(g.date))
+        .attr('y', (g) => yScale(g.hours))
+        .attr('height', (g) => height - yScale(g.hours))
+        .attr('width', xScale.bandwidth())
+        .style('fill', 'blue')
+
+      svg
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', -(height / 2) - margin)
+      .attr('y', margin / 2.4)
+      .attr('transform', 'rotate(-90)')
+      .attr('text-anchor', 'middle')
+      .text('No of hours worked (hrs)')
+
+    svg.append('text')
+      .attr('class', 'label')
+      .attr('x', width / 2 + margin)
+      .attr('y', height + margin * 1.7)
+      .attr('text-anchor', 'middle')
+      .text('Working Days')
+
+    svg.append('text')
+      .attr('class', 'title')
+      .attr('x', width / 2 + margin)
+      .attr('y', 40)
+      .attr('text-anchor', 'middle')
+      .text('Number of hours worked by denny per day')
+
+  
+  }
      render() {
-           return <svg ref={node => this.node = node}
+           return( 
+           <div> 
+           <button onClick={this.createDailyBarChart}>daily</button>
+           <button onClick={this.createMonthlyBarChart}>monthly</button>
+
+          <svg ref={node => this.node = node}
            width={800} height={500}>
            </svg>
+
+           </div>
+           );
         }
      }
      export default BarChart;
